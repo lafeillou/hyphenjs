b6356a31539363355bc66999afcf5b82e919fd2d
yanguangjie <yanguangjie@gomo.com>
2018-01-19 11:46:29 +0800

    update dev-watch

--- dev-watch.js
+++ dev-watch.js
@@ -1,31 +1,31 @@
-const {
-    exec
-} = require('child_process');
+const exec = require('child_process').exec;
 const chokidar = require('chokidar');
 const path = require('path');
 const devPath = path.resolve(__dirname, 'src');
 
+// prevent exec `npm run build` when add file first time.
 let preventExec = true;
+
 // watch src file change to auto build.
 const watcher = chokidar.watch(devPath, {
     ignored: /node_modules|\.git|dist/
 }).on('all', (event, path) => {
-    if(preventExec) return;
+    if (preventExec) return;
     log(event, path, 'start build');
     exec('npm run build', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
         }
-        log(blue('Build in dist'));
+        log(blue('build in dist'));
     });
 }).on('ready', () => {
     preventExec = false;
-    log('Begin to watch');
-})
+    log('begin to watch');
+});
 
 function log(...args) {
-    console.log(blue('(Dev-watch)') + ': ' + args.join(' '));
+    console.log(blue('(Dev-watch): ') + args.join(' '));
 }
 
 function blue(str) {
