2d20bec4079f4632b18aa6a4849b62d56d86be3c
Gotjoy <18814128243@163.com>
2018-01-28 14:23:07 +0800

    clean

--- dev-watch.js
+++ dev-watch.js
@@ -3,15 +3,11 @@ const exec = require('child_process').exec
 const srcPath = path.resolve(__dirname, 'src')
 const bs = require('browser-sync').create('dev-watch')
 
-log('Start watching...')
-
 bs.watch('./index.html').on('change', bs.reload)
 bs.watch(srcPath).on('change', () => {
-    log('Start building...')
     bs.notify("Compiling, please wait!")
     exec('npm run build', (error, stdout, stderr) => {
         if (error) return;
-        log('Built in dist.')
         bs.reload()
         bs.notify("Built in dist.")
     })
@@ -20,11 +16,3 @@ bs.watch(srcPath).on('change', () => {
 bs.init({
     server: true
 })
-
-function log(...args) {
-    console.log(blue('(Dev-watch): ') + args.join(' '))
-}
-
-function blue(str) {
-    return '\x1b[1m\x1b[34m' + str + '\x1b[39m\x1b[22m'
-}
